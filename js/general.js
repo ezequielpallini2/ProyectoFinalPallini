de42743814ba8361099c9ef246a954e34a61ef7b
Ezequiel Pallini <ezequielpallini2@gmail.com>
2023-08-02 13:46:42 -0300

    actualizaciones

--- js/general.js
+++ js/general.js
@@ -2,6 +2,7 @@ const arrayeventos = [
     {
       id : "1",
       imagen: "images/kurt.png",
+      stock : 10000,
       description: "Reviven a Kurt Cobain y Nirvana vuelve a la Argentina",
       date: "Primavera 2023",
       price : "8500"
@@ -9,6 +10,7 @@ const arrayeventos = [
     {
       id : 2,
       imagen: "images/dillom.png",
+      stock : 10000,
       description: "Dillom destrozará el Movistar Arena con misiles Exocet",
       date: "Primavera 2023",
       price : "85600"
@@ -16,6 +18,7 @@ const arrayeventos = [
     {
       id : 3,
       imagen: "images/mona.png",
+      stock : 10000,
       description: "La Mona Gimenez toca en las Islas Malvinas",
       date: "Primavera 2023",
       price : "80500"
@@ -23,6 +26,7 @@ const arrayeventos = [
     {
       id : 4,
       imagen: "images/futbol.png",
+      stock : 10000,
       description: "Lanús y Banfield se disputan el campeonato cerradura",
       date: "Primavera 2023",
       price : "82500"
@@ -30,6 +34,7 @@ const arrayeventos = [
     {
       id : 5,
       imagen: "images/moria.png",
+      stock : 10000,
       description: "Moria Casan presenta: Terminator 2 llega al teatro",
       date: "Primavera 2023",
       price : "84800"
